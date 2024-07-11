d5159560ba0ec91d716940982f6ded32d262f0bc
Shota Kubota <ta2@o2p.jp>
2024-07-11 23:02:23 +0900

    feat(eslint/configs/react): simplify React ESLint configuration

--- eslint/configs/react.mjs
+++ eslint/configs/react.mjs
@@ -1,25 +1,11 @@
-import react from "eslint-plugin-react";
-import reactJsxRuntime from "eslint-plugin-react/configs/jsx-runtime.js";
-import reactRecommended from "eslint-plugin-react/configs/recommended.js";
+import reactJsxRuntimeConfig from "eslint-plugin-react/configs/jsx-runtime.js";
+import reactRecommendedConfig from "eslint-plugin-react/configs/recommended.js";
 
 export default [
+  reactRecommendedConfig,
+  reactJsxRuntimeConfig,
+
   {
-    files: ["**/*.{jsx,tsx}"],
-
-    languageOptions: {
-      ...reactRecommended.languageOptions,
-      ...reactJsxRuntime.languageOptions,
-    },
-
-    plugins: {
-      react,
-    },
-
-    rules: {
-      ...reactRecommended.rules,
-      ...reactJsxRuntime.rules,
-    },
-
     settings: {
       react: {
         version: "detect",
