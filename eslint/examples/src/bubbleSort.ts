0b8a0d762de504ade06b377f0ed9debaa716dbe8
Shota Kubota <ta2@o2p.jp>
2024-07-12 15:38:13 +0900

    chore(eslint/examples): add TypeScript sample code

---
+++ eslint/examples/src/bubbleSort.ts
@@ -0,0 +1,13 @@
+export function bubbleSort(array: number[]): void {
+  const lentgh = array.length;
+
+  for (let i = 0; i < lentgh; i++) {
+    for (let j = 0; j < lentgh - i - 1; j++) {
+      if (array[j] > array[j + 1]) {
+        const temp = array[j];
+        array[j] = array[j + 1];
+        array[j + 1] = temp;
+      }
+    }
+  }
+}
