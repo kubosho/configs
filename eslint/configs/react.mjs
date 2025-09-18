8fea8c3abcbcf6a606f641847ffba250627f663e
KUBOTA Shota <ta2+github@o2p.jp>
2025-09-18 17:41:35 +0900

    refactor(eslint): use extends field in JSX files lint rules

--- eslint/configs/react.mjs
+++ eslint/configs/react.mjs
@@ -1,19 +1,14 @@
 import reactPlugin from 'eslint-plugin-react';
+import { defineConfig } from 'eslint/config';
 
-export default [
+export default defineConfig([
   {
-    ...reactPlugin.configs.flat.recommended,
-    ...reactPlugin.configs.flat['jsx-runtime'],
-
     files: ['**/*.{jsx,mjsx,tsx,mtsx}'],
-    rules: {
-      ...reactPlugin.configs.flat.recommended.rules,
-      ...reactPlugin.configs.flat['jsx-runtime'].rules,
-    },
+    extends: [reactPlugin.configs.flat.recommended, reactPlugin.configs.flat['jsx-runtime']],
     settings: {
       react: {
         version: 'detect',
       },
     },
   },
-];
+]);
